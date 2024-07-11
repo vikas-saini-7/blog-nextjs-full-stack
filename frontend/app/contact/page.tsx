6d2fc25133806e05d6f571744022063cb5deb3ab
vikas-saini-7 <vikas289888@gmail.com>
2024-07-11 14:30:01 +0530

    updated footer links

---
+++ frontend/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="min-h-[90vh]">
+      <div className="container mx-auto py-8">Contact</div>
+    </div>
+  );
+};
+
+export default page;
--- frontend/components/common/Footer.tsx
+++ frontend/components/common/Footer.tsx
@@ -1,32 +1,43 @@
-import { IconBrandGithub, IconBrandLinkedin, IconBrandTwitch, IconBrandTwitter, IconCopyright } from '@tabler/icons-react'
-import Link from 'next/link'
-import React from 'react'
+import {
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconBrandTwitch,
+  IconBrandTwitter,
+  IconCopyright,
+} from "@tabler/icons-react";
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='bg-black text-white'>
-      <div className='container mx-auto flex items-center justify-between p-12'>
+    <div className="bg-black text-white">
+      <div className="container mx-auto flex items-center justify-between p-12">
         <div>
-          <h1  style={{ letterSpacing: '1px' }} className='-mb-2 font-medium text-4xl font-primary uppercase'>Vikas Saini blog</h1>
-          <p className='uppercase text-xs'>Design | Tech | Marketing</p>
+          <h1
+            style={{ letterSpacing: "1px" }}
+            className="-mb-2 font-medium text-4xl font-primary uppercase"
+          >
+            Vikas Saini blog
+          </h1>
+          <p className="uppercase text-xs">Design | Tech | Marketing</p>
         </div>
-        <div className='grid grid-cols-2 gap-2 gap-x-8'>
-          <Link href={'#'}>page</Link>
-          <Link href={'#'}>page</Link>
-          <Link href={'#'}>page</Link>
-          <Link href={'#'}>page</Link>
+        <div className="grid grid-cols-2 gap-2 gap-x-8">
+          <Link href={"/contact"}>Contact</Link>
+          <Link href={"/sitemap"}>Sitemap</Link>
         </div>
-        <div className='flex gap-4'>
-          <IconBrandLinkedin size={32}/>
-          <IconBrandTwitter size={32}/>
-          <IconBrandGithub size={32}/>
+        <div className="flex gap-4">
+          <IconBrandLinkedin size={32} />
+          <IconBrandTwitter size={32} />
+          <IconBrandGithub size={32} />
         </div>
       </div>
-      <div className='container mx-auto border-t border-gray-500 flex items-center justify-between p-4'>
-        <p className='text-xs uppercase flex items-center gap-1'><IconCopyright size={16}/> vikas saini blog 2024</p>
+      <div className="container mx-auto border-t border-gray-500 flex items-center justify-between p-4">
+        <p className="text-xs uppercase flex items-center gap-1">
+          <IconCopyright size={16} /> vikas saini blog 2024
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
